36742e9b96d5603c69c9a9a25928a9231161b23f
HeineConK <eggnog@gmx.de>
2025-12-08 14:21:22 +0100

    fix erroneous km++ weight init

--- R/kmeanspp_init.R
+++ R/kmeanspp_init.R
@@ -1,17 +1,17 @@
 kmpp_init <- function(X, k = 3){
   N <- nrow(X)
   W <- matrix(nc = ncol(X), nr = k)
-  
+
   idx_all <- 1:N
   idx_w <- numeric()
-  
-  weights <- rep(1:N, 1)
-  
+
+  weights <- rep(1, N)
+
   for(i in 1:k){
     idx_selectable <- setdiff(idx_all, idx_w)
     idx_next_w <- sample(idx_selectable,1, prob = weights[idx_selectable])
     idx_w <- append(idx_w, idx_next_w)
-    
+
     W <- X[idx_w, ]
     if(i == 1){
       weights <- rowSums(sweep(X,2,W)^2)
@@ -23,6 +23,6 @@ kmpp_init <- function(X, k = 3){
       weights <- dists
     }
   }
-  
-  return(W) 
+
+  return(W)
 }
